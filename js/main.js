8e6693b5db7377ba270888491355ebb345724577
huhncares-cmd <69119083+huhncares-cmd@users.noreply.github.com>
2020-08-20 18:24:01 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,20 @@
 let login = ""
 let p = 0
-function right1(){
-				$: p = "|"
-				localStorage.setItem("p", p)
+function right1(){    
+	$: p = "|"    	
+	localStorage.setItem("p", p)
 }
-function wrong1(){
-				$: p = ""
-				localStorage.setItem("p", p)
+function wrong1(){    
+	$: p = ""    	
+	localStorage.setItem("p", p)
 }
-function right(){
-				p = localStorage.getItem("p")
-				$: p = p + "|"
-				localStorage.setItem("p", p)
+function right(){     	
+	p = localStorage.getItem("p")    	
+	$: p = p + "|"    	
+	localStorage.setItem("p", p)
 }
-function wrong(){
-				p = localStorage.getItem("p")
-				$: p = p
-				localStorage.setItem("p", p)
-}
-
- 
+function wrong(){     	
+	p = localStorage.getItem("p")    	
+	$: p = p    	
+	localStorage.setItem("p", p)
+}   
