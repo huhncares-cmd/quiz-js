20b22af3e3fa2de15420123172483773f3133f81
huhncares-cmd <69119083+huhncares-cmd@users.noreply.github.com>
2020-08-20 17:57:12 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,22 @@
+let login = ""
+let p = 0
+function right1(){
+				$: p = "|"
+				localStorage.setItem("p", p)
+}
+function wrong1(){
+				$: p = ""
+				localStorage.setItem("p", p)
+}
+function right(){
+				p = localStorage.getItem("p")
+				$: p = p + "|"
+				localStorage.setItem("p", p)
+}
+function wrong(){
+				p = localStorage.getItem("p")
+				$: p = p
+				localStorage.setItem("p", p)
+}
+
+ 
