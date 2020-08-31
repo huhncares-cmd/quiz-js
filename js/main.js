eb269c3a57a942e6fcc23cb733d7ebf9e4a16ab5
huhncares-cmd <69119083+huhncares-cmd@users.noreply.github.com>
2020-08-31 20:02:34 +0200

    Update main.js
    added an alert if a answer ist correct

--- js/main.js
+++ js/main.js
@@ -1,19 +1,23 @@
 let login = ""
 let p = 0
 function right1(){    
+	alert("Yeah, right!")
 	$: p = "|"    	
 	localStorage.setItem("p", p)
 }
-function wrong1(){    
+function wrong1(){  
+	alert("Oh no, wrong!")
 	$: p = ""    	
 	localStorage.setItem("p", p)
 }
-function right(){     	
+function right(){   
+	alert("Yeah, right!")
 	p = localStorage.getItem("p")    	
 	$: p = p + "|"    	
 	localStorage.setItem("p", p)
 }
-function wrong(){     	
+function wrong(){   
+	alert("Oh no, wrong!")
 	p = localStorage.getItem("p")    	
 	$: p = p    	
 	localStorage.setItem("p", p)
